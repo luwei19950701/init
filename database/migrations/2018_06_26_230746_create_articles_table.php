7aa835d5875061865a9f79ba7aecb07196e3c5e8
Lingance <lingtima@gmail.com>
2019-10-23 23:03:53 +0800

    init

---
+++ database/migrations/2018_06_26_230746_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id');
+            $table->integer('user_id');
+            $table->string('slug');
+            $table->string('title');
+            $table->string('sub_title');
+            $table->text('content');
+            $table->timestamps();
+            $table->timestamp('published_at')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
